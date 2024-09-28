6b898da6e9ba6e16b856982afe7d570064507c13
Amr69130 <amrouchenorris@gmail.com>
2024-09-28 19:37:49 +0200

    ajout de texte à droite de la carte

--- lib/pages/map_monde.dart
+++ lib/pages/map_monde.dart
@@ -32,17 +32,49 @@ class _MapMondeState extends State<MapMonde> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: InteractiveViewer(
-        panEnabled: true, // Permet de déplacer l'image
-        minScale: 0.5, // Échelle minimale
-        maxScale: 4.0, // Échelle maximale
-        child: Image.asset(
-        'assets/images/familles-lng-map-monde.png',
-        fit: BoxFit.contain, // Assure que l'image soit bien contenue
-      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 7, // 70% de l'écran pour l'image interactive
+            child: InteractiveViewer(
+              panEnabled: true, // Permet de déplacer l'image
+              minScale: 0.5, // Échelle minimale
+              maxScale: 4.0, // Échelle maximale
+              child: Image.asset(
+                'assets/images/familles-lng-map-monde.png',
+                fit: BoxFit.contain, // Assure que l'image soit bien contenue
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3, // 30% de l'écran pour les informations
+            child: Container(
+              color: Colors.white, // Optionnel : mettre un fond blanc
+              padding: EdgeInsets.all(16.0), // Espacement intérieur
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Informations sur la carte',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10), // Espacement entre le titre et le texte
+                  Text(
+                    "Cette carte et beaucoup d'informations proviennent du site web de M. Jacques LECLERC (https://www.axl.cefan.ulaval.ca/index.html)",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  // Tu peux ajouter plus de widgets ici (boutons, icônes, etc.)
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
